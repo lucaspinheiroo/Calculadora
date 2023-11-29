3863c78418f9989266fac4a6a575056cdaafdb8a
Lucas Pinheiro <131314634+lucaspinheiroo@users.noreply.github.com>
2023-11-28 23:47:34 -0400

    Métodos adicionados

--- Calculadora/Entities/Operacoes.cs
+++ Calculadora/Entities/Operacoes.cs
@@ -8,5 +8,36 @@ namespace Calculadora.Entities
 {
     internal class Operacoes
     {
+        public decimal Valor1 { get; set; }
+        public decimal Valor2 { get; set; }
+
+        public Operacoes(decimal valor1, decimal valor2)
+        {
+            Valor1 = valor1;
+            Valor2 = valor2;
+        }
+
+
+        public decimal Adicao()
+        {
+            return Valor1 + Valor2;
+        }
+
+        public decimal Subtracao()
+        {
+            return Valor1 - Valor2;
+        }
+
+        public decimal Multiplicacao()
+        {
+            return (Valor1 * Valor2);
+        }
+
+        public decimal Divisao()
+        {
+            return (Valor1 / Valor2);
+        }
     }
+
+
 }
